03223acc82b8d3980ac0b511e2e9c65e63c301a6
WayJam So <imsuwj@gmail.com>
2022-01-16 20:34:14 +0800

    update onedark options

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,7 @@ return {
     {
         "navarasu/onedark.nvim",
         config = function()
-            require("plugins.onedark").setup()
+            require("plugins.onedark").config()
         end
     },
     {
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,10 +1,16 @@
-local function setup()
-    vim.g.onedark_diagnostics_undercurl = false
-    vim.g.onedark_darker_diagnostics = false
-    vim.g.onedark_disable_toggle_style = true
-    require("onedark").setup()
-    local colors = require("onedark.colors")
-    vim.cmd("au VimEnter * highlight Pmenu guibg=" .. colors.bg0)
+local function config()
+    require("onedark").setup({
+        style = 'dark',
+        transparent = false,
+        term_colors = true,
+        diagnostics = {
+            darker = false,
+            undercurl = false,
+        }
+    })
+    require('onedark').load()
+    -- local colors = require("onedark.colors")
+    -- vim.cmd("au VimEnter * highlight Pmenu guibg=" .. colors.bg0)
 end
 
-return {setup = setup}
+return {config = config}
