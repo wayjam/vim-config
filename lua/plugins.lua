79cdef597bc83d162ecffbe746f6d104b2b70926
WayJam So <imsuwj@gmail.com>
2022-11-04 16:52:31 +0800

    fix null-ls load order

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -141,7 +141,6 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
-    after = "mason.nvim",
     config = function() require("plugins.null-ls").config() end,
   },
   {
