036a7fed381808db767c83920d653b29cd9bb54b
WayJam So <imsuwj@gmail.com>
2024-05-29 20:18:42 +0800

    update .lucrc.json

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,9 +1,6 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
+  init = function() end,
   opts = {},
 }
