60bd9e141b142418d0dec6af0eef072a729c1da4
WayJam So <imsuwj@gmail.com>
2025-01-13 21:14:36 +0800

    add render markdown

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -4,4 +4,27 @@ return {
     "petobens/poet-v",
     ft = { "python" },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      vim.api.nvim_create_user_command(
+        "RenderMarkdownToggle",
+        function(_) require("render-markdown").toggle() end,
+        { nargs = 0 }
+      )
+    end,
+  },
 }
