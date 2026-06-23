534762817b01460ea539d8302f0baf9842055738
WayJam So <imsuwj@gmail.com>
2026-06-23 17:00:04 +0800

    use `edge` colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
--- Active colorscheme: onedark. Kept as a single return for simplicity.
+-- Kept as a single return for simplicity.
 -- Add alternatives as new files if you want to swap (lazy will only load
 -- what is imported).
-return {
+
+local onedark = {
   "navarasu/onedark.nvim",
   lazy = false,
   priority = 1000,
@@ -31,3 +32,31 @@ return {
     },
   },
 }
+
+local edge = {
+  "sainnhe/edge",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.edge_enable_italic = true
+    vim.g.edge_style = "default"
+    vim.g.edge_transparent_background = 1
+    vim.g.edge_current_word = "high contrast background"
+    vim.cmd.colorscheme "edge"
+  end,
+  specs = {
+    {
+      "nvim-lualine/lualine.nvim",
+      optional = true,
+      opts = {
+        options = {
+          theme = "edge",
+        },
+      },
+    },
+  },
+}
+
+return edge
